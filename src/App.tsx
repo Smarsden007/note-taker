9af67eedc22133e7f36b019f39b4c2e1ce87f93e
Smarsden007 <seannmarsden@gmail.com>
2022-08-12 21:09:55 -0700

    killing project and re-running

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,26 @@
 import React from "react";
-import { useDispatch } from "react-redux";
-import { useSelector } from "react-redux";
-import { addNote } from "./action";
 import { NewNoteInput } from "./NewNoteInput";
+import { useSelector, useDispatch } from "react-redux";
 import { NoteState } from "./notesReducer";
+import { addNote } from "./action";
 
 function App() {
   const notes = useSelector<NoteState, NoteState["notes"]>(
     (state) => state.notes
   );
-    const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
-  const onAddNote = (note:string) => {
-    dispatch(addNote(note))
-  }
+  const onAddNote = (note: string) => {
+    dispatch(addNote(note));
+  };
 
   return (
     <>
-      <NewNoteInput addNote={onAddNote}/>
+      <NewNoteInput addNote={onAddNote} />
       <hr />
       <ul>
         {notes.map((note) => {
-         return <li key={note}>{note}</li>;
+          return <li key={note}>{note}</li>;
         })}
       </ul>
     </>
