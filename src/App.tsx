8e4b33d7e47cc41675088247d6f48eb151f3aebd
Smarsden007 <seannmarsden@gmail.com>
2022-08-12 21:39:20 -0700

    styling app

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,9 @@ import { NewNoteInput } from "./NewNoteInput";
 import { useSelector, useDispatch } from "react-redux";
 import { NoteState } from "./notesReducer";
 import { addNote } from "./action";
-
+import { MasterContainer, NoteBox, SubContainer, List} from "./styles/NoteStyles";
+import { Container, Section } from "./globalStyles";
+import {RiTodoLine} from 'react-icons/ri'
 function App() {
   const notes = useSelector<NoteState, NoteState["notes"]>(
     (state) => state.notes
@@ -16,13 +18,21 @@ function App() {
 
   return (
     <>
+      <SubContainer>
+      <MasterContainer>
+      <RiTodoLine size={200}/>
       <NewNoteInput addNote={onAddNote} />
       <hr />
+      
       <ul>
         {notes.map((note) => {
-          return <li key={note}>{note}</li>;
+          return <NoteBox><List key={note}>{note}</List></NoteBox>;
         })}
       </ul>
+      
+      
+      </MasterContainer>
+     </SubContainer>
     </>
   );
 }
---
+++ src/styles/NoteStyles.js
@@ -0,0 +1,32 @@
+import styled from 'styled-components';
+
+
+
+export const NoteBox = styled.div`
+width: 70vw;
+height: auto;
+background-color: #333333;
+color: white;
+font-size: 2rem;
+padding: 2rem;
+margin: 2rem;
+border-radius: 10px;
+`
+
+export const MasterContainer = styled.div`
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+padding: 5rem;
+
+`
+export const SubContainer = styled.div`
+display: flex;
+flex-direction: column;
+justify-content: center;
+`
+
+export const List = styled.li`
+    list-style: none;
+`
